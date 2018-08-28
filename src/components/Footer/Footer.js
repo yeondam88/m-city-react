67671b7f3f154df152d9b73a74507478c1441554
Yeondam Park <giyen9088@gmail.com>
2018-08-27 20:58:20 -0700

    header styles and adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Footer extends Component {
-  render() {
-    return <div>Footer</div>;
-  }
-}
+import { CityLogo } from "../UI/Icons";
+
+const Footer = () => {
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo link linkTo="/" width="70px" height="70px" />
+      </div>
+      <div className="footer_discl">
+        Manchester City 2018. All rights reserved.
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
-Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
+import { CityLogo } from "../UI/Icons";
 
 class Header extends Component {
   render() {
@@ -19,7 +20,9 @@ class Header extends Component {
       >
         <Toolbar style={{ display: "flex" }}>
           <div style={{ flexGrow: 1 }}>
-            <div class="header_logo">Logo</div>
+            <div class="header_logo">
+              <CityLogo linkTo="/" link width="70px" height="70px" />
+            </div>
           </div>
           <Link to="/the_team">
             <Button color="inherit">The team</Button>
